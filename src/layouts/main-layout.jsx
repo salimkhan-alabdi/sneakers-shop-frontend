fada3d6e3018a5c28d5fb3d79905d3966099f31c
Temurkan <famill1949@gmail.com>
2026-01-30 20:44:07 +0500

    added scroll to top component

--- src/layouts/main-layout.jsx
+++ src/layouts/main-layout.jsx
@@ -1,13 +1,15 @@
-import Footer from '@/components/shared/footer'
-import Navbar from '@/components/shared/navbar'
-import { Outlet } from 'react-router-dom'
+import ScrollToTop from "@/components/ScrollToTop";
+import Footer from "@/components/shared/footer";
+import Navbar from "@/components/shared/navbar";
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Navbar />
       <Outlet />
       <Footer />
     </div>
-  )
+  );
 }
